8b1254b58d105589fd4d05430774c3317d0c6d10
Thomas Dahlberg <thomas.dahlberg@cygni.se>
2019-12-01 08:15:35 +0100

    solved day01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -3,7 +3,19 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
-        
+        string[] lines = System.IO.File.ReadAllLines("input"); 
+        int amount = 0;
+        foreach(string line in lines) {
+            amount =  amount + GetFuelAmount(Int32.Parse(line));
+        }
+        Console.WriteLine(amount);
+    }
+
+    public static int GetFuelAmount(int mass, int currentFuel = 0) {
+        int requiredFule = (int)Math.Floor((decimal)mass/3)-2;
+        if(requiredFule <= 0) {
+            return currentFuel;
+        }
+        return GetFuelAmount(requiredFule, requiredFule + currentFuel);
     }
 }
